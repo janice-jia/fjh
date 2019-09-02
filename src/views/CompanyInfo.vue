cf2e17491fd019e3e1d4dad1fffca9270bce0814
xxxidaizy zhg <xxxidaizyzhg@xxxidaizydeMacBook-Pro.local>
2019-09-03 01:06:52 +0800

    公司航线详情样式完成

--- src/views/CompanyInfo.vue
+++ src/views/CompanyInfo.vue
@@ -173,27 +173,99 @@
         </el-tabs>
       </div>
       <!-- 甲板导航 -->
-      <div class="detail detail-play">
-        <div class="detail-title">邮轮玩乐</div>
-        <el-tabs v-model="activeName2">
-          <el-tab-pane :label="'船上娱乐\n14个'" name="first">自助餐厅</el-tab-pane>
-          <el-tab-pane :label="'运动健身\n5个'" name="second">
-            <div class="detail-item">
-              <el-row type="flex">
-                <el-col class="item-info" v-for="(item,index) in 3" :key="index">
-                  <div class="image">
-                    <img src />
+      <div class="detail detail-nav">
+        <div class="detail-title">甲板导航</div>
+        <el-tabs v-model="activeName3">
+          <el-tab-pane label="5层" name="first">
+            <div class="detail-detail">
+              <div class="image">
+                <img src />
+              </div>
+              <div class="info">
+                <div class="info-item">
+                  <div class="title">船舱类型</div>
+                  <div class="detail">
+                    <div class="detail-item">
+                      <p>内舱房</p>
+                      <div class="room">DD</div>
+                    </div>
+                    <div class="detail-item">
+                      <p>海景房</p>
+                      <div class="room">CF</div>
+                      <div class="room">CD</div>
+                    </div>
                   </div>
-                  <p class="info-title">按摩池</p>
-                  <p>楼层：9 | 容纳：268 | 消费：免费</p>
-                  <p>红房子西餐厅为游客提供中式及西式套餐</p>
-                </el-col>
-              </el-row>
+                </div>
+                <div class="info-item">
+                  <div class="title">邮轮玩乐</div>
+                  <div class="detail">
+                    <div class="detail-item">
+                      <p>无</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="info-item">
+                  <div class="title">甲板图例</div>
+                  <div class="detail">
+                    <div class="detail-item">
+                      <p>残疾人舱房</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </el-tab-pane>
-          <el-tab-pane :label="'海上休闲\n10个'" name="third">酒吧</el-tab-pane>
-          <el-tab-pane :label="'其他\n2个'" name="third">酒吧</el-tab-pane>
+          <el-tab-pane label="6层" name="second">6层</el-tab-pane>
+          <el-tab-pane label="7层" name="third">7层</el-tab-pane>
+          <el-tab-pane label="8层" name="fourth">8层</el-tab-pane>
+          <el-tab-pane label="9层" name="fifth">9层</el-tab-pane>
+          <el-tab-pane label="10层" name="sixth">10层</el-tab-pane>
+          <el-tab-pane label="11层" name="seventh">11层</el-tab-pane>
+          <el-tab-pane label="12层" name="eighth">12层</el-tab-pane>
+          <el-tab-pane label="13层" name="nine">13层</el-tab-pane>
         </el-tabs>
+      </div>
+      <!-- 常见问题 -->
+      <div class="detail detail-problem">
+        <div class="detail-title">常见问题</div>
+        <div class="content">
+          <div class="item" v-for="(item,index) in 3" :key="index">
+            <div class="question">
+              <div class="red tag">问</div>
+              <div class="text">多大的儿童可以乘坐游轮？如何选择舱房？</div>
+            </div>
+            <div class="anwser">
+              <div class="yellow tag">答</div>
+              <div
+                class="text"
+              >6个月以上的儿童都可以乘坐邮轮旅行。请留意具体产品的优惠说明。6个月以上的儿童都可以乘坐邮轮旅行。请留意具体产品的优惠说明。6个月以上的儿童都可以乘坐邮轮旅行。请留意具体产品的优惠说明。</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 精选游记 -->
+      <div class="detail detail-select">
+        <div class="detail-title">精选游记</div>
+        <div class="detail-item">
+          <el-row type="flex" :gutter="30">
+            <el-col :span="8" v-for="(item,index) in 3" :key="index">
+              <div class="item-info">
+                <div class="image">
+                  <img src />
+                </div>
+                <div class="center">
+                  <div class="photo">
+                    <img src />
+                  </div>
+                  <p class="username">潘潘三条子</p>
+                  <p class="time">2015-03-20</p>
+                  <p class="title">深度探秘双子星丽星邮轮</p>
+                  <p class="with">和谁：朋友</p>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
       </div>
     </div>
   </div>
@@ -212,6 +284,7 @@ export default {
       activeIndex: 1,
       activeName: "fourth",
       activeName2: "second",
+      activeName3: "first",
       tableData: [
         {
           img: "",
@@ -353,11 +426,10 @@ export default {
   background: #ffffff;
   padding-left: 0;
   padding-right: 0;
+  padding-bottom: 50px;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.35);
   .detail {
-    padding-top: 100px;
-    padding-left: 1px;
-    padding-right: 1px;
+    padding: 100px 1px 0 1px;
     .detail-title {
       font-size: 36px;
       text-align: center;
@@ -460,6 +532,169 @@ export default {
   }
   .detail-play .el-tabs .el-tabs__header {
     padding-left: 200px;
+  }
+  .detail-nav .el-tabs {
+    .el-tabs__header {
+      padding-left: 185px;
+    }
+    .el-tabs__item {
+      padding: 0 25px;
+      margin-bottom: 10px;
+    }
+    .detail-detail {
+      height: 720px;
+      width: 100%;
+      margin-top: 40px;
+      .image,
+      .info {
+        height: 100%;
+        display: inline-block;
+        float: left;
+        background-color: #ffffff;
+        border: 1px solid #e5e5e5;
+      }
+      .image {
+        width: 770px;
+        max-height: 720px;
+        overflow-y: scroll;
+        img {
+          max-width: 100%;
+        }
+      }
+      .info {
+        width: 398px;
+        padding: 0 30px;
+        margin-left: 30px;
+        max-height: 720px;
+        overflow-y: scroll;
+        .info-item {
+          margin-top: 30px;
+          width: 100%;
+          .title {
+            font-size: 24px;
+            color: #333333;
+            margin-bottom: 20px;
+          }
+          .detail {
+            width: 100%;
+            display: inline-block;
+            padding-top: 0;
+            .detail-item {
+              margin-right: 60px;
+              float: left;
+              p {
+                font-size: 20px;
+                color: #666666;
+                margin-bottom: 10px;
+              }
+              .room {
+                width: 40px;
+                height: 30px;
+                line-height: 30px;
+                font-size: 20px;
+                text-align: center;
+                color: #ffffff;
+                background: #ee6b03;
+                margin-right: 20px;
+                display: inline-block;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .detail-problem {
+    .content {
+      border: 1px solid #e5e5e5;
+      width: 100%;
+      max-height: 600px;
+      overflow-y: scroll;
+      margin-top: 40px;
+      padding: 30px;
+      .item {
+        margin-bottom: 60px;
+        width: 100%;
+        .question,
+        .anwser {
+          margin-bottom: 20px;
+          font-size: 24px;
+          color: #333333;
+          width: 100%;
+          .tag {
+            display: inline-block;
+            width: 30px;
+            height: 30px;
+            font-size: 24px;
+            color: #ffffff;
+            line-height: 30px;
+            text-align: center;
+            float: left;
+            margin-right: 10px;
+            &.red {
+              background: #ee6b03;
+            }
+            &.yellow {
+              background: #f7ae0a;
+            }
+          }
+          .text {
+            display: inline-block;
+            width: calc(100% - 40px);
+          }
+        }
+        .anwser {
+          font-size: 20px;
+        }
+      }
+    }
+  }
+  .detail-select {
+    .detail-item {
+      margin-top: 60px;
+      .item-info {
+        border: 2px solid #e5e5e5;
+        padding-bottom: 10px;
+        .image {
+          width: 100%;
+          height: 210px;
+          background: #e5e5e5;
+          img {
+            max-width: 100%;
+          }
+        }
+        .center {
+          text-align: center;
+          margin-top: 60px;
+          position: relative;
+          .photo {
+            width: 90px;
+            height: 90px;
+            border-radius: 50%;
+            background: #f7ae0a;
+            position: absolute;
+            top: -105px;
+            left: 140px;
+            img {
+              max-width: 100%;
+            }
+          }
+          p{
+            margin-bottom: 5px;
+          }
+          .username {
+            font-size: 20px;
+          }
+          .time,.with{
+            font-size: 14px;
+            color: #999999;
+          }
+          .title {
+            font-size: 24px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
